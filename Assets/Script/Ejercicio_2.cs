dd3d612579ca6c6977a773f58e540a4cd35919d3
AxelSalama <axelsalama2006@gmail.com>
2022-03-28 11:54:16 -0300

    Ejercicio 1 a 5

---
+++ Assets/Script/Ejercicio_2.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*Realizá un programa que, siendo num1 y num2 variables enteras con valores 4 y 5, 
+ respectivamente, realice la operación correspondiente y muestre el resultado en pantalla: 
+a. Suma 
+b. Producto 
+*/
+public class Ejercicio_2 : MonoBehaviour
+{
+    int num1 = 4;
+    int num2 = 5;
+    int res;
+    // Start is called before the first frame update
+    void Start()
+    {
+        num1 += num2;
+        Debug.Log(res);
+
+        num1 *= num2;
+        Debug.Log(res);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Script/Ejercicio_5.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// Realizá un programa que, dados dos números enteros num1 y num2 cargados desde  el Inspector 
+//muestre el resultado de la división del primero por el segundo en la consola siguiendo el formato num1 + “ / “ + num2 + “ = “ + resultado.  
+//En el caso de que el segundo valor num2 sea cero, mostrar el siguiente mensaje de error:  “No se puede dividir por cero”. 
+
+public class Ejercicio_5 : MonoBehaviour
+{
+    public int n1;
+    public int n2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        int res;
+
+        if (n2 == 0)
+        {
+            Debug.Log("Error, no se puede dividir por cero");
+        }
+        else
+        {
+            res = n1 / n2;
+
+            Debug.Log(n1 + "/" + n2 + "=" + res);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
